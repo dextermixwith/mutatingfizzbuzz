86cd54cdd42d764553ada91419ae254b1da57373
James Barker and John Armstrong-Prior <development+barker1889+dextermixwith@esendex.com>
2013-11-27 13:35:24 +0000

    Initial commit.

---
+++ MutatingFizzBuzz.Tests/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿namespace MutatingFizzBuzz.Tests
+{
+    public class FizzBuzz
+    {
+        public string GetOutputFor(int number)
+        {
+            if (number == 3)
+            {
+                return "Fizz";
+            }
+
+            if (number == 5)
+            {
+                return "Buzz";
+            }
+
+            if (number == 15)
+            {
+                return "FizzBuzz";
+            }
+
+            return "1";
+        }
+    }
+}
---
+++ MutatingFizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NinjaTurtles;
+
+namespace MutatingFizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        public void FizzBuzzGivesCorrectOutputFor(int number, string expectedOutput)
+        {
+            Assert.That(new FizzBuzz().GetOutputFor(number), Is.EqualTo(expectedOutput));
+        }
+    }
+
+    [TestFixture]
+    public class FizzBuzzMutationTests
+    {
+        [Test, MutationTest]
+        public void GivesCorrectOutputForMutationTests()
+        {
+            MutationTestBuilder<FizzBuzz>.For("GetOutputFor").Run();
+        }
+        
+    }
+
+}
